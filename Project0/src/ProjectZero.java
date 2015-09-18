c33810a69ac255c565dc43bf3f789bd4c9ecc186
Freddymac32 <Freddymac1020@gmail.com>
2015-09-18 00:24:54 -0400

    Update ProjectZero.java

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,3 +1,7 @@
+/* I let the user decide the type of nums so I can do the sums.
+In reality I'm treating doubles like an "error" and correcting it if need be. **/
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProjectZero {
@@ -5,18 +9,37 @@ public class ProjectZero {
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
+			Integer input1 = null, input2 = null, result;
+			double num1 = 0.0, num2 = 0.0, decimalResult;
 			
 			System.out.print("Please enter two numbers of your exalted choosing: ");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
 			
+			try{ input1 = inputSource.nextInt();}
+			
+			catch ( InputMismatchException a ){
+				 num1 = inputSource.nextDouble();
+					System.out.println("The number you chose is: " + num1);
+					}
+			
+			try {input2 = inputSource.nextInt();}
+			catch (InputMismatchException a){
+				num2 = inputSource.nextDouble();
+				System.out.println("The number you chose is: " + num2);
+
+			}
+			
+			if (input1 != null){
 			result = input1 + input2;
-			System.out.println(result);
+			System.out.println("Your sum is: " + result);
+			}
 			
+			else{
+				decimalResult = num1 + num2;
+				System.out.println("Your sum is: " + decimalResult);
+	}
+
 			inputSource.close();
 			
 	}
 
 }
-
