95b0b19f9dbf7f7d287277aef970555f2abb4f20
Freddymac32 <Freddymac1020@gmail.com>
2015-09-18 00:11:19 -0400

    Update ProjectZero.java
    Not finished at all, thought I could get by with my previous version from spring but it was absolutely terrible. Will update as much as possible even though I will definitely get a poor grade on this one.

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,3 +1,7 @@
+/** Project 0! Going to try and use try and catch exception errors to 
+ * sums these nums seamlessly**/
+ 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProjectZero {
@@ -5,18 +9,38 @@ public class ProjectZero {
 	public static void main(String[] args) {
 		
 			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
+			int input1 = 0, input2 = 0, result;
+			double num1, num2, decimalResult;
 			
 			System.out.print("Please enter two numbers of your exalted choosing: ");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
 			
+			try{ 
+				input1 = inputSource.nextInt();
+				System.out.println ("The number you chose is: " + input1);
+			}
+			
+			catch ( InputMismatchException a ){
+				 num1 = inputSource.nextDouble();
+					System.out.println("You're using the decimal number " + num1);
+					}
+			
+			try {
+				input2 = inputSource.nextInt();
+				System.out.println ("The number you chose is: " + input2);
+			}
+			catch (InputMismatchException a){
+				num2 = inputSource.nextDouble();
+				System.out.println("You're using the decimal number " + num2);
+
+			}
+			
+		
 			result = input1 + input2;
 			System.out.println(result);
-			
+		
+
 			inputSource.close();
 			
 	}
 
-}
 
